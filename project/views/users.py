7b7665e35b702bf196731ebb27cdde41acc10000
andrew <andrewmar78@yandex.ru>
2022-06-12 17:02:34 +0500

    small changings

--- project/views/users.py
+++ project/views/users.py
@@ -1,14 +1,44 @@
 #  Недоработан
+import jwt
 from flask import request
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, abort
 
+from helpers.decorators import auth_required
+from project.constants import JWT_ALGORITHM, JWT_SECRET
+from project.container import auth_service
+from project.exceptions import NoUserFound
+from project.services import user_service, UserService
+from project.setup_db import db
+from project.views.auth import user_schema
 
 user_ns = Namespace('users')
 
 
 @user_ns.route('/')
 class UsersView(Resource):
+    @auth_required
+    def get(self, refresh_token):
+        """Получение токена, данных, обновление данных"""
+        try:
+            auth_data = request.headers['Authorization']
+            token = auth_data.split("Bearer ")[-1]
+            data = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
+            email = data.get("email")
+
+            user = UserService(db.session).get_user_by_email(email)
+            return user_schema.dump(user), 200
+        except NoUserFound:
+            abort(404, 'User is not found')
+
     def post(self):
-        user_data = request.json
-        user = user_service.create_user(user_data)
-        return user, 201
+        try:
+            auth_data = request.headers['Authorization']
+            token = auth_data.split("Bearer ")[-1]
+            data = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
+            email = data.get("email")
+
+            new_data = user_schema.dump(request.json)
+            UserService(db.session).update_user(new_data, email)
+            return "", 200
+        except NoUserFound:
+            abort(404, 'User is not found')
